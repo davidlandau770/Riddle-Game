3a7950eb20c56428d0325cabd5d4a2515aee4551
davidlandau770 <dal778770@gmail.com>
2025-07-20 20:34:10 +0300

    continue player

--- service/player.service.js
+++ service/player.service.js
@@ -11,13 +11,32 @@ const usernameRegistration = async () => {
     return inputName;
 }
 
-const addPlayer = async () => {
-    const obj = {
-        name: await usernameRegistration(),
-        lowestTime: p.lowestTime()
+const getBestTime = async (name, p) => {
+    const lowestTime = p.lowestTime();
+    const response = await getPlayers();
+    const findPlayer = response.find(player => player.username === name);
+    const bestTimeResponse = findPlayer ? findPlayer.best_time : 0;
+    const result = Math.min(...[bestTimeResponse, lowestTime].filter(x => typeof x === 'number'));
+    console.log("result: ***");
+    console.log(result, lowestTime);
+    if (result > 0) {
+        console.log(`Your record is: ${result}\nGood luck!`);
+        return result;
     }
-    console.log("obj: ");
-    console.log(obj);
+    else {
+        // console.log("This is your first time!\nGood luck!")
+        return null
+    };
+}
+
+const addPlayer = async () => {
+    const name = await usernameRegistration();
+    const p = new Player(name);
+    const bestTime = await getBestTime(name, p);
+    const obj = {
+        name: name,
+        best_time: bestTime
+    }
     const response = await fetch(`${URL}/players/addPlayer`, {
         headers: {
             'Content-Type': 'application/json',
@@ -27,21 +46,46 @@ const addPlayer = async () => {
     })
     const body = await response.json();
     console.log(body);
+    return p;
 }
 
-const getPlayer = async () => {
+const getPlayers = async () => {
     return await fetch(`${URL}/players`).then((res) => res.json());
 }
 
+const updatePlayer = async (name, p) => {
+    let bestTime;
+    try {
+        bestTime = await getBestTime(name, p);
+    } catch (err) {
+        console.error(`bestTime: ${err.message}`)
+    }
+    console.log("bestTime: ");
+    console.log(bestTime, name);
+    const obj = {
+        best_time: bestTime
+    }
+    const response = await fetch(`${URL}/players/updatePlayer/${name}`, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        method: 'PUT',
+        body: JSON.stringify(obj)
+    })
+    const body = await response.json();
+    console.log(body);
+}
+
 const viewLeaderboard = async () => {
-    const players = await getPlayer();
-    const lowestPlayers = players.sort((a, b) => a.lowestTime - b.lowestTime).splice(0,3)
-    console.log(lowestPlayers);
+    const players = await getPlayers();
+    const bestPlayers = players.sort((a, b) => a.best_time - b.best_time).splice(0, 3)
+    console.log(bestPlayers);
 }
 
 export {
     usernameRegistration,
     addPlayer,
-    getPlayer,
+    getPlayers,
+    updatePlayer,
     viewLeaderboard
 }
